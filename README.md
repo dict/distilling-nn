1ccd62cc29dfcfa19992f7d44852f3a83e3bb83f
dict <pkccr1@gmail.com>
2017-04-19 11:23:19 +0000

    distilling nn with Resnet

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # distilling-nn
 Tensorflow implementation of [Distilling the Knowledge in a Neural Network][1]
 
+Base model : Resnet
+Binary code(Windows PE) classification for variant malware detection, ensemble with distilling neural network
+
 
   [1]: https://www.cs.toronto.edu/~hinton/absps/distillation.pdf
+
---
+++ models/resnet_utils.py
@@ -0,0 +1,190 @@
+import tensorflow as tf
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.training import moving_averages
+
+MOVING_AVERAGE_DECAY = 0.9997
+BN_DECAY = MOVING_AVERAGE_DECAY
+BN_EPSILON = 0.001
+CONV_WEIGHT_DECAY = 0.00004
+CONV_WEIGHT_STDDEV = 0.1
+FC_WEIGHT_DECAY = 0.00004
+FC_WEIGHT_STDDEV = 0.01
+RESNET_VARIABLES = 'resnet_variables'
+UPDATE_OPS_COLLECTION = 'resnet_update_ops'
+IMAGENET_MEAN_BGR = [103.062623801, 115.902882574, 123.151630838, ]
+
+activation = tf.nn.relu
+
+def stack(x, c):
+    for n in range(c['num_blocks']):
+        s = c['stack_stride'] if n == 0 else 1
+        c['block_stride'] = s
+        with tf.variable_scope('block%d' % (n + 1)):
+            x = block(x, c)
+    return x
+
+
+def block(x, c):
+    filters_in = x.get_shape()[-1]
+
+    m = 4 if c['bottleneck'] else 1
+    filters_out = m * c['block_filters_internal']
+
+    shortcut = x 
+
+    c['conv_filters_out'] = c['block_filters_internal']
+
+    if c['bottleneck']:
+        with tf.variable_scope('a'):
+            c['ksize'] = 1
+            c['stride'] = c['block_stride']
+            x = conv(x, c)
+            x = bn(x, c)
+            x = activation(x)
+
+        with tf.variable_scope('b'):
+            x = conv(x, c)
+            x = bn(x, c)
+            x = activation(x)
+
+        with tf.variable_scope('c'):
+            c['conv_filters_out'] = filters_out
+            c['ksize'] = 1
+            assert c['stride'] == 1
+            x = conv(x, c)
+            x = bn(x, c)
+    else:
+        with tf.variable_scope('A'):
+            c['stride'] = c['block_stride']
+            assert c['ksize'] == 3
+            x = conv(x, c)
+            x = bn(x, c)
+            x = activation(x)
+            #x = tf.nn.dropout(x, 0.5)
+
+        with tf.variable_scope('B'):
+            c['conv_filters_out'] = filters_out
+            assert c['ksize'] == 3
+            assert c['stride'] == 1
+            x = conv(x, c)
+            x = bn(x, c)
+
+    with tf.variable_scope('shortcut'):
+        if filters_out != filters_in or c['block_stride'] != 1:
+            c['ksize'] = 1
+            c['stride'] = c['block_stride']
+            c['conv_filters_out'] = filters_out
+            shortcut = conv(shortcut, c)
+            shortcut = bn(shortcut, c)
+
+    return activation(x + shortcut)
+
+
+def bn(x, c):
+    return tf.contrib.layers.batch_norm(x, decay=0.9, center=True, scale=True, epsilon=True, activation_fn=tf.nn.relu)
+    """
+    x_shape = x.get_shape()
+    params_shape = x_shape[-1:]
+
+    if c['use_bias']:
+        bias = _get_variable('bias', params_shape,
+                             initializer=tf.zeros_initializer)
+        return x + bias
+
+
+    axis = list(range(len(x_shape) - 1))
+
+    beta = _get_variable('beta',
+                         params_shape,
+                         initializer=tf.zeros_initializer)
+    gamma = _get_variable('gamma',
+                          params_shape,
+                          initializer=tf.zeros_initializer)
+
+    moving_mean = _get_variable('moving_mean',
+                                params_shape,
+                                initializer=tf.zeros_initializer,
+                                trainable=False)
+    moving_variance = _get_variable('moving_variance',
+                                    params_shape,
+                                    initializer=tf.zeros_initializer,
+                                    trainable=False)
+
+    mean, variance = tf.nn.moments(x, axis)
+    update_moving_mean = moving_averages.assign_moving_average(moving_mean,
+                                                               mean, BN_DECAY)
+    update_moving_variance = moving_averages.assign_moving_average(
+        moving_variance, variance, BN_DECAY)
+    tf.add_to_collection(UPDATE_OPS_COLLECTION, update_moving_mean)
+    tf.add_to_collection(UPDATE_OPS_COLLECTION, update_moving_variance)
+
+    mean, variance = control_flow_ops.cond(
+        c['is_training'], lambda: (mean, variance),
+        lambda: (moving_mean, moving_variance))
+
+    x = tf.nn.batch_normalization(x, mean, variance, beta, gamma, BN_EPSILON)
+
+    return x
+    """
+
+
+def fc(x, c):
+    num_units_in = x.get_shape()[1]
+    num_units_out = c['fc_units_out']
+    weights_initializer = tf.truncated_normal_initializer(
+        stddev=FC_WEIGHT_STDDEV)
+
+    weights = _get_variable('weights',
+                            shape=[num_units_in, num_units_out],
+                            initializer=weights_initializer,
+                            weight_decay=FC_WEIGHT_STDDEV)
+    biases = _get_variable('biases',
+                           shape=[num_units_out],
+                           initializer=tf.zeros_initializer)
+    x = tf.nn.xw_plus_b(x, weights, biases)
+    return x
+
+
+def _get_variable(name,
+                  shape,
+                  initializer,
+                  weight_decay=0.0,
+                  dtype='float',
+                  trainable=True):
+    "A little wrapper around tf.get_variable to do weight decay and add to"
+    "resnet collection"
+    if weight_decay > 0:
+        regularizer = tf.contrib.layers.l2_regularizer(weight_decay)
+    else:
+        regularizer = None
+    collections = [tf.GraphKeys.GLOBAL_VARIABLES, RESNET_VARIABLES]
+    return tf.get_variable(name,
+                           shape=shape,
+                           initializer=initializer,
+                           dtype=dtype,
+                           regularizer=regularizer,
+                           collections=collections,
+                           trainable=trainable)
+
+
+def conv(x, c):
+    ksize = c['ksize']
+    stride = c['stride']
+    filters_out = c['conv_filters_out']
+
+    filters_in = x.get_shape()[-1]
+    shape = [ksize, ksize, filters_in, filters_out]
+    initializer = tf.truncated_normal_initializer(stddev=CONV_WEIGHT_STDDEV)
+    weights = _get_variable('weights',
+                            shape=shape,
+                            dtype='float',
+                            initializer=initializer,
+                            weight_decay=CONV_WEIGHT_DECAY)
+    return tf.nn.conv2d(x, weights, [1, stride, stride, 1], padding='SAME')
+
+
+def _max_pool(x, ksize=3, stride=2):
+    return tf.nn.max_pool(x,
+                          ksize=[1, ksize, ksize, 1],
+                          strides=[1, stride, stride, 1],
+                          padding='SAME')
